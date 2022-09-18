f7de778a3f81209aaac9ef832efbbc326ca14766
Dorjee <rishav345@gmail.com>
2022-09-18 12:05:23 -0400

    secondcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AddChar from "./Input";
+import React, { useState } from "react";
+import Header from "./Header";
 
 function App() {
+  //dummy data
+  const [data, setData] = useState(
+    {
+      id: Date.now(), 
+      name: "Luke Skywalker",
+      BirthDate: "19BBY",
+      Height: "172cm",
+      Mass: "77kg",
+      Homeworld: "Tatooine",
+      Species: "Human",
+    },
+    {
+      id: Date.now(), 
+      name: "C-3PO",
+      BirthDate: "112BBY",
+      Height: "167cm",
+      Mass: "75kg",
+      Homeworld: "Tatooine",
+      Species: "Droid",
+    },
+  );
+
+  const charRows = (
+    <tbody>
+       <tr>
+      <td>{data.name}</td>
+      <td>{data.BirthDate}</td>
+      <td>{data.Height}</td>
+      <td>{data.Mass}</td>
+      <td>{data.Homeworld}</td>
+      <td>{data.Species}</td>
+      </tr>
+    </tbody>
+    
+    
+  );
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="table-responsive">
+      <Header />
+      <AddChar />
+      <table caption="page 1" className="table table-bordered">
+        <tbody className="table-dark">
+          <tr>
+            <th>Name</th>
+            <th>Birth Date</th>
+            <th>Height</th>
+            <th>Mass</th>
+            <th>Homeworld</th>
+            <th>Species</th>
+          </tr>
+        </tbody>
+        {charRows}
+      </table>
     </div>
   );
 }
---
+++ src/Input.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AddChar() {
+  return (
+    <div>
+      <form>
+        <input />
+        <button type="button" className="btn btn-info">
+          Search
+        </button>
+        <button type="button" className="btn btn-warning">
+          Clear
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AddChar;
